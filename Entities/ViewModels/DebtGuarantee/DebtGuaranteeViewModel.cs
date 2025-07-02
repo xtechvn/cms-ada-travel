c6182bf7262b5c62b2625d190fa085f496a1e9e1
Hiếu <anhhieuk50@gmail.com>
2025-07-02 15:29:46 +0700

    update

--- Entities/ViewModels/DebtGuarantee/DebtGuaranteeViewModel.cs
+++ Entities/ViewModels/DebtGuarantee/DebtGuaranteeViewModel.cs
@@ -26,6 +26,7 @@ namespace Entities.ViewModels.DebtGuarantee
         public double Payment { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
 
     }
 }
