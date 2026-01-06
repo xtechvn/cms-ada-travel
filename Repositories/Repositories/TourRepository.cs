95c2cc16dc0763ebec9fb3d94c19d45500de6e7d
Hiếu <62824420+anhhieuk16@users.noreply.github.com>
2026-01-06 11:52:09 +0700

    update tourr

--- Repositories/Repositories/TourRepository.cs
+++ Repositories/Repositories/TourRepository.cs
@@ -821,20 +821,24 @@ namespace Repositories.Repositories
                         tourDAL.CreateMultipleTourDestination(TourDestinations);
                     }
                 }
-                foreach (var item in model.ListTourProgramPackages)
+                if (model.ListTourProgramPackages != null && model.ListTourProgramPackages.Count() > 0)
                 {
-                    if (item.Id <= 0)
+                    foreach (var item in model.ListTourProgramPackages)
                     {
-                        item.TourProductId = tour_id;
-                        item.CreatedBy = model.CreatedBy;
-                        item.CreatedDate = DateTime.Now;
-                        item.UpdatedBy = model.CreatedBy;
-                        item.UpdatedDate = DateTime.Now;
-                        tourProductDAL.CreateTourPackages(item);
- 
+                        if (item.Id <= 0)
+                        {
+                            item.TourProductId = tour_id;
+                            item.CreatedBy = model.CreatedBy;
+                            item.CreatedDate = DateTime.Now;
+                            item.UpdatedBy = model.CreatedBy;
+                            item.UpdatedDate = DateTime.Now;
+                            tourProductDAL.CreateTourPackages(item);
+
+                        }
+
                     }
-                    
                 }
+            
                 return tour_id;
             }
             catch
